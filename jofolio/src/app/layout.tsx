4c5b11497f35dc4e5c0c678e2372faf82fbd467e
Jo Zhou <jozhou.uxui@gmail.com>
2024-12-23 17:20:31 +1100

    Updated footer structure

--- jofolio/src/app/layout.tsx
+++ jofolio/src/app/layout.tsx
@@ -58,20 +58,28 @@ function Navbar() {
 
 function Footer() {
   return (
-    <div className="flex flex-col">
+    <div className="flex flex-col items-center rounded-t-[72px] bg-background pt-48">
 		
-		<div className="flex flex-col items-center border-t border-primary-50 px-6 py-8 lg:px-16 lg:py-16 mx-4 md:mx-16 lg:mx-36">
 
-			<span className="text-2xl md:text-3xl font-bold uppercase text-primary-400 mb-8">Jo Zhou</span>
-				
-			<div className="flex flex-row flex-wrap space-x-4">
-				<SocialMediaIcon iconNumber={0} />
-				<SocialMediaIcon iconNumber={1} />
-				<SocialMediaIcon iconNumber={2} />
+
+			<span className="text-2xl md:text-3xl font-bold text-primary mb-8">Tell me about your next project</span>
+
+			<div className="flex flex-row">
+				<a href="/resume" className="cursor-pointer rounded-full bg-primary-400 text-white text-xs lg:text-base px-4 lg:px-8 py-2">Email me</a>
+				<a href="/resume" className="cursor-pointer rounded-full bg-primary-400 text-white text-xs lg:text-base px-4 lg:px-8 py-2">Text me</a>
+			</div>
+			
+			
+			{/* Insert horizontal line here */}
+
+
+			<div className="flex flex-row w-full  px-20 justify-between">
+				<span>2024 All rights reserved.</span>
+				<span>LinkedIn / Behance</span>
 
 			</div>
 
-	  	</div>
+
 
     </div>
   );
